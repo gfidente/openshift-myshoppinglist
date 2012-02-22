35b213633ab04d22049fafea8f9bf5cc9c82b97c
giulivo <giulivo@localhost.localdomain>
2012-02-22 11:20:54 +0100

    Initial import

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='MyShoppingList',
+      version='1.0',
+      description='Mobile Shopping List App',
+      author='Giulio Fidente',
+      author_email='gfidente@redhat.com',
+      url='http://www.python.org/sigs/distutils-sig/',
+      install_requires=['Flask', 'pymongo'],
+     )
---
+++ wsgi/static/myshoppinglist.js
@@ -0,0 +1,3 @@
+function clean(){
+ $("#cleanform").submit();
+}
